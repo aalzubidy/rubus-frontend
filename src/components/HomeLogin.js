505d080d00c4d544fb0a34f3a1262094ca70f5b1
Ahmed Al-Zubidy <aalzubidy90@gmail.com>
2021-01-03 16:30:16 -0800

    Added login feature

--- src/components/HomeLogin.js
+++ src/components/HomeLogin.js
@@ -18,11 +18,22 @@ class HomeLogin extends Component {
         })
     }
 
+    emailValidation = (email) => {
+        let validEmail = false;
+        email = email ? email.trim().toLowerCase() : email;
+        if (email) {
+            const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+            validEmail = emailRegex.test(email);
+        }
+
+        return validEmail
+    }
+
     render() {
         return (
             <div>
-                {this.state.showLoginForm ? <LoginForm /> : <RegisterForm />}
                 <button onClick={this.setShowLoginForm}>{this.state.showLoginForm ? 'Register Form' : 'Login Form'}</button>
+                {this.state.showLoginForm ? <LoginForm emailValidation={this.emailValidation} /> : <RegisterForm />}
             </div>
         );
     }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,15 +3,46 @@ import React, { Component } from 'react'
 class LoginForm extends Component {
     constructor(props) {
         super(props);
-        // this.state = { }
+        this.state = {
+            email: '',
+            password: '',
+            validEmail: false
+        }
     }
-    render() { 
+
+    handleOnChange = (evt) => {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        });
+
+        if (evt.target.name === 'email') {
+            this.setState(() => {
+                return {
+                    validEmail: this.props.emailValidation(evt.target.value)
+                }
+            })
+        }
+    }
+
+    handleSubmit = (evt) => {
+        evt.preventDefault();
+        // axois to login
+        console.log(this.state);
+    }
+
+    render() {
         return (
             <div>
-              Login Form  
+                <form onSubmit={this.handleSubmit}>
+                    <input type='text' value={this.state.email} name='email' onChange={this.handleOnChange} placeholder='Email...' />
+                    <br />
+                    <input type='password' value={this.state.password} name='password' onChange={this.handleOnChange} placeholder='Password...' />
+                    <br />
+                    <button disabled={!this.state.validEmail}>Login</button>
+                </form>
             </div>
         );
     }
 }
- 
+
 export default LoginForm;
